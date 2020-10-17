a384b1f23485ccfb6733718eb5ab078eef1ca18e
rishitha957 <46604699+rishitha957@users.noreply.github.com>
2020-10-18 02:09:01 +0530

    renamed output script

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from get_all_api_calls import automatic_api_deprecation_detection as ADepD
 path_list = ['./scikit-learn/sklearn/','./numpy/numpy/','./pandas/pandas/','./scipy/scipy/',
 				'./matplotlib/','./seaborn/seaborn/']
 
-file2 = open("deprecated_using_decorator.txt","w")
+file2 = open("output.txt","w")
 
 for path in path_list:
 	package = path.split("/")[-2]
